efc235d1072ac8bbdf0748859fc9cc662d369d73
宇髄 天元 <60231586+DonorKrovi@users.noreply.github.com>
2023-09-10 17:02:44 +0300

    Update ServerPlayerMixin.java

--- src/mixin/java/net/kyori/adventure/platform/fabric/impl/mixin/ServerPlayerMixin.java
+++ src/mixin/java/net/kyori/adventure/platform/fabric/impl/mixin/ServerPlayerMixin.java
@@ -24,7 +24,6 @@
 package net.kyori.adventure.platform.fabric.impl.mixin;
 
 import com.google.common.collect.MapMaker;
-import io.netty.channel.Channel;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
@@ -140,12 +139,10 @@ public abstract class ServerPlayerMixin extends PlayerMixin implements Forwardin
   @Inject(method = "restoreFrom", at = @At("RETURN"))
   private void copyData(final ServerPlayer old, final boolean alive, final CallbackInfo ci) {
     FabricServerAudiencesImpl.forEachInstance(controller -> controller.bossBars().replacePlayer(old, (ServerPlayer) (Object) this));
-    final @Nullable ConnectionAccess conn = ((ConnectionAccess) this.connection.connection);
-    if (conn == null) return;
-
-    final Channel chan = conn.getChannel();
-    if (chan == null) return;
-    chan.attr(FriendlyByteBufBridge.CHANNEL_RENDER_DATA).set(this);
+    final var channel = ((ConnectionAccess) this.connection.connection).getChannel();
+    if (channel != null) {
+      channel.attr(FriendlyByteBufBridge.CHANNEL_RENDER_DATA).set(this);
+    }
   }
 
   @Inject(method = "disconnect", at = @At("RETURN"))
